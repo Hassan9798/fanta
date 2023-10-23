59b6086e028349caec59d177046d9a6e57bae1a8
m.jalil <m.jalil@mythod.com>
2023-10-23 12:00:56 +0500

    first_commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <Text>Fanta</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
