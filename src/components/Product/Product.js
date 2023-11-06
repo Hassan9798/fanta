cffa48565a3b00eb5f7922c500bbad3519d5fbbc
Hassan Ali <h.ali@mythod.com>
2023-11-06 12:20:09 +0500

    product screen completed

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,7 +1,9 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
+import { Dimensions, Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { icons, images } from '../../constants/images'
 
+
+const {width} = Dimensions.get('window');
 const Product = ({item,height,width,style}) => {
   const arr=[
     icons.removeIcon,
@@ -9,7 +11,7 @@ const Product = ({item,height,width,style}) => {
   ]
   return (
     <View style={[styles.container,style]}>
-    <Image source={item.img} resizeMode='cover' style={{height:height??"100%",width:width??"100%"}} />
+    <Image source={item.img} resizeMode='contain' style={{height:height??"100%",width:width??"100%"}} />
     <Text style={styles.title}>{item.name}</Text>
     <Text style={styles.desc}>{"250ml"}</Text>
     <View style={styles.price}>
@@ -19,7 +21,7 @@ const Product = ({item,height,width,style}) => {
     <View style={{flexDirection:'row'}}>
     {arr.map((item,index)=>
     <View style={[styles.bottom,{borderLeftWidth:index === 0?2:0}]}>
-      <Image source={item} resizeMode='cover' style={{height:30,width:30}}/>
+      <Image source={item} resizeMode='cover' style={{height:24,width:24}}/>
     </View>
     )  
   }
@@ -37,7 +39,7 @@ const styles = StyleSheet.create({
         borderBottomWidth:2,
         borderColor:"#DEDEDE",
         height:450,
-        width:200,
+        width:(width/2)-5,
         borderRadius:2,
         paddingLeft:10,
         justifyContent:'flex-start',
@@ -61,7 +63,7 @@ const styles = StyleSheet.create({
     },
     bottom:{
       height:50,
-      width:88,
+      width:(((width/2)-5)/2)-12,
       borderColor:"#DEDEDE",
       borderRightWidth:2,
       borderTopWidth:2,
--- src/screens/BrandProduct/BrandProduct.js
+++ src/screens/BrandProduct/BrandProduct.js
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text, ScrollView, Dimensions } from 'react-native'
 import Header from '../../components/Header/Header';
 import React from 'react'
 import Brands from '../../components/Brands/Brands';
@@ -7,6 +7,8 @@ import { images } from '../../constants/images';
 import { FlatList } from 'react-native';
 
 const BrandProduct = ({route,navigation}) => {
+
+  const {width} = Dimensions.get('window');
   const {brandName}=route.params;
   const products=[
     {id:1,img:images.product1,name:"7UP"},
@@ -20,20 +22,24 @@ const BrandProduct = ({route,navigation}) => {
      <View style={{marginBottom:20}}>
      <FlatList
      data={products}
+     style={{
+      marginHorizontal:5,
+     }}
      renderItem={({item,index})=>
      <Product 
      item={item} 
      index={index} 
      length={products.length} 
      style={{
-      marginLeft:index===0 || Number.isInteger(index/2)?6:"",
+      // marginLeft:index===0 || Number.isInteger(index/2)?6:"",
       borderLeftWidth:index===0 || Number.isInteger(index/2)?2:0,
       borderBottomWidth:index===products.length-1 ||index === products.length-2?2:0,
-      marginRight:!Number.isInteger(index/2)?5:0
+      // marginRight:!Number.isInteger(index/2)?5:0
       }
       }
-      height={220}
-      width={180}
+  
+      height={200}
+      width={"90%"}
      />
     }
     
